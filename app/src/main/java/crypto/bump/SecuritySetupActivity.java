2181a38d5e12286923b622b3db4969e238269763
TheRebelOfBabylon <legrosjunior@gmail.com>
2017-11-12 17:40:39 -0500

    Initial commit

---
+++ app/src/main/java/crypto/bump/SecuritySetupActivity.java
@@ -0,0 +1,13 @@
+package crypto.bump;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecuritySetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_setup);
+    }
+}
